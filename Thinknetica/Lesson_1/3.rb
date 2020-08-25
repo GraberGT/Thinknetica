3ecb05a60bb1c00a32d0b7a06a467c19b6aa0b1d
GraberGT <rogerpluscom@gmail.com>
2020-08-25 19:25:51 +0600

    upd

--- Thinknetica/Lesson_1/3.rb
+++ Thinknetica/Lesson_1/3.rb
@@ -1,28 +1,13 @@
 # frozen_string_literal: true
 
-puts 'First side'
-a = gets.chomp.to_i
-
-puts 'Second side'
-b = gets.chomp.to_i
-
-puts 'Third side'
-c = gets.chomp.to_i
-
-array = [a, b, c]
-hyp = array.sort![2]
-k1 = array[0]
-k2 = array[1]
-sum = k1 + k2
-
-right = hyp**2 == k1**2 + k2**2
-
-if right
-  puts 'This triangle is right angled'
-elsif hyp && k1 == k2
-  puts 'This is right angled triangle and equilaterial'
-elsif (sum == hyp) && (k1 == k2 || k1 == hyp || hyp == k2)
-  puts 'This is triangle with equal sides but not right angled'
+puts 'Enter three sides'
+a, b, c = [gets.to_i, gets.to_i, gets.to_i].sort
+if a == b && b == c
+  puts 'Equilateral'
+elsif a == b || b == c
+  puts 'Isosceles'
+elsif c**2 == a**2 + b**2
+  puts 'Rectangular'
 else
-  puts 'This is regular triangle'
+  puts 'Arbitrary'
 end
