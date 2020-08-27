64fe66b1ada90a2236fd8699daf34b32790171ff
GraberGT <rogerpluscom@gmail.com>
2020-08-27 18:31:44 +0600

    upd

--- Thinknetica/Lesson_2/4.rb
+++ Thinknetica/Lesson_2/4.rb
@@ -6,13 +6,7 @@ vowels = %w[a e i o u y]
 result = {}
 
 alphabet.each.with_index(1) do |letter, index|
-  if vowels.include? letter
-    result[letter] = index
-  end
+  result[letter] = index if vowels.include? letter
 end
 
 puts result
-
-
-
-
